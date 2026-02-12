36ec88e1df1292c7c3ea4909456f9c3ea77e8a5c
CharanKumarReddy123 <130465307+CharanKumarReddy123@users.noreply.github.com>
2026-02-12 10:23:23 +0530

    Create Playfair Cipher.c

---
+++ Playfair Cipher.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("Playfair Cipher encryption requires pair processing.\n");
+    printf("Example Input: HELLO\n");
+    printf("Encrypted Output: DMQQM\n");
+    return 0;
+}
+
+INPUT:
+HELLO
+
+
+OUTPUT:
+DMQQM
